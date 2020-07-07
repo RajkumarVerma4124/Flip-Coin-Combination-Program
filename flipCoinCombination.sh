c9722a277de78c55513a2edad6aadde4b5966293
RAJ <vermar4142@gmail.com>
2020-07-07 23:59:09 +0530

    Added:-Added the simulator file to display head or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -4,3 +4,14 @@ echo "Flip Coin Simulation Program "
 echo "This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
 printf "\n"
 
+echo "As a Simulator started with Flipping a Cointo Display Heads or Tails (UC1)"
+
+flipCoin=$(( $RANDOM % 2 +1 ))
+echo $flipCoin    
+if [ $flipCoin -eq 1 ]
+then
+	echo "Heads"
+else
+	echo "Tails"
+fi
+
