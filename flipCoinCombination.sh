b5250956ba5e16866f6758aa9c5718f2c4bb4f66
RAJ <vermar4142@gmail.com>
2020-07-08 00:16:13 +0530

    Added:-Added the file for storing the Triplet combination and shows its Percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -111,3 +111,101 @@ echo "percentage Of HH Is : $percentDoubleHeadCount % "
 echo "percentage Of TT Is : $percentDoubleTailCount % "
 echo "percentage Of HT Is : $percentSingleHeadTailCount % "
 echo "percentage Of TH Is : $percentSingleTailHeadCount % "
+
+printf "\n"
+echo "As a simulator, do the same for Triplet Combination. (UC4)"
+printf "\n"
+
+read -p "Enter the time you want to flip a coin : " flipCount
+
+HHHCount=0
+HHTCount=0
+HTHCount=0
+HTTCount=0
+THHCount=0
+THTCount=0
+TTHCount=0
+TTTCount=0
+
+
+declare -A displayCoinTriplet
+
+for ((i=0;i<$flipCount;i++))
+do
+	flipCoin[$i]=$(($RANDOM%8 + 1))
+	if [ ${flipCoin[$i]} -eq 1 ]
+	then
+	displayCoinTriplet[$i]="HHH"
+	HHHCount=$(($HHHCount+1))
+	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HHHCount
+	elif [ ${flipCoin[$i]} -eq 2 ]
+	then
+	displayCoinTriplet[$i]="HHT"
+	HHTCount=$(($HHTCount+1))
+	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HHTCount
+	elif [ ${flipCoin[$i]} -eq 3 ]
+	then
+	displayCoinTriplet[$i]="HTH"
+	HTHCount=$(($HTHCount+1)) 
+	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HTHCount
+	elif [ ${flipCoin[$i]} -eq 4 ]
+	then
+	displayCoinTriplet[$i]="HTT"
+	HTTCount=$(($HTTCount+1))
+	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HTTCount
+	elif [ ${flipCoin[$i]} -eq 5 ]
+	then
+	displayCoinTriplet[$i]="THH"
+	THHCount=$(($THHCount+1))
+	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $THHCount
+	elif [ ${flipCoin[$i]} -eq 6 ]
+	then
+	displayCoinTriplet[$i]="THT"
+	THTCount=$(($THTCount+1))
+	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $THTCount
+	elif [ ${flipCoin[$i]} -eq 7 ]
+	then
+	displayCoinTriplet[$i]="TTH"
+	TTHCount=$(($TTHCount+1)) 
+	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $TTHCount
+	elif [ ${flipCoin[$i]} -eq 8 ]
+	then
+	displayCoinTriplet[$i]="TTT"
+	TTTCount=$(($TTTCount+1))
+	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $TTTCount
+	fi  
+done
+
+echo "Displaying the Outcomes of flipped coins : " ${flipCoin[*]}
+echo "Displaying the Doublets of flipped coins : " ${displayCoinTriplet[*]}
+
+
+echo "Count of HHH Is : "$HHHCount
+echo "Count of HHT Is : "$HHTCount
+echo "Count of HTH Is : "$HTHCount
+echo "Count of HTT Is : "$HTTCount
+echo "Count of THH Is : "$THHCount
+echo "Count of THT Is : "$THTCount
+echo "Count of TTH Is : "$TTHCount
+echo "Count of TTT Is : "$TTTCount
+
+
+
+percentOfHHH=$(($HHHCount*100 / $flipCount));
+percentOfHHT=$(($HHTCount*100 / $flipCount));
+percentOfHTH=$(($HTHCount*100 / $flipCount));
+percentOfHTT=$(($HTTCount*100 / $flipCount));
+percentOfTHH=$(($THHCount*100 / $flipCount));
+percentOfTHT=$(($THTCount*100 / $flipCount));
+percentOfTTH=$(($TTHCount*100 / $flipCount));
+percentOfTTT=$(($TTTCount*100 / $flipCount));
+
+
+echo "percent Of HHH IS : $percentOfHHH % "
+echo "percent Of HHT IS : $percentOfHHT % "
+echo "percent Of HTH IS : $percentOfHTH % "
+echo "percent Of HTT IS : $percentOfHTT % "
+echo "percent Of THH IS : $percentOfTHH % "
+echo "percent Of THT IS : $percentOfTHT % "
+echo "percent Of TTH IS : $percentOfTTH % "
+echo "percent Of TTT IS : $percentOfTTT % "
