b7528dc83e50a157c7624997f83decda94f353e4
RAJ <vermar4142@gmail.com>
2020-07-08 00:36:16 +0530

    Added:-Added the file for sorting the Singlet Doublet & Triplet combination and shows its Winning Computation

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -53,6 +53,22 @@ echo "Heads Percentage is : $percentHeads % "
 echo "Tails Percentage is : $percentTails % "
 printf "\n"
 
+sortSinglet=$( printf "%s\n" ${flipCoin[*]} | sort -n )
+sortdisplayCoinSinglet=$( printf "%s\n" ${displayCoinSinglet[*]} | sort )
+
+echo "Displaying Outcomes Of Coins flipped is After Sorting  : " $sortSinglet
+echo "Displaying The singlet of Coins Flipped After Sorting  : " ${sortdisplayCoinSinglet[*]}
+printf "\n"
+
+if [ $headCount -gt $tailCount -a $percentHeads -gt $percentTails ] 
+then 
+	echo "Head Wins with count of : " $headCount "And Percentage Of : " $percentHeads  
+elif [ $tailCount -gt $headCount -a $percentTails -gt $percentHeads ] 
+then 
+	echo "Tail Wins with count of : " $tailCount  "And Percentage Of : " $percentTails
+fi 
+
+printf "\n"
 
 echo "As a simulator, loop to create a Doublet Combination. Store the doublet combination as well as the percentage." 
 echo "Doublet Combinations are HH, HT, TH, and TT (UC3)"
@@ -113,6 +129,49 @@ echo "percentage Of HT Is : $percentSingleHeadTailCount % "
 echo "percentage Of TH Is : $percentSingleTailHeadCount % "
 
 printf "\n"
+
+sortDoublet=$( printf "%s\n" ${flipCoin[*]} | sort -n )
+sortdisplayCoinDoublet=$( printf "%s\n" ${displayCoinDoublet[*]} | sort )
+echo "Displaying Outcomes Of Coins flipped is After Sorting  : " $sortDoublet
+echo "Displaying The Doublet of Coins Flipped After Sorting  : " ${sortdisplayCoinDoublet[*]}
+printf "\n"
+
+declare -A doubletArray
+doubletArray[1]=$doubleHeadCount
+doubletArray[2]=$doubleTailCount
+doubletArray[3]=$singleHeadTailCount
+doubletArray[4]=$singleTailHeadCount
+max=${doubletArray[1]}
+for (( i=1;i<5;i++ ))
+do
+	if [ ${doubletArray[$i]} -gt $max ]
+	then
+       		max=${doubletArray[$i]}
+  	fi
+done
+  
+if [ $max -eq ${doubletArray[1]} ]
+then
+	echo "HH Combination Wins with count of : " $doubleHeadCount   "And Percentage Of : " $percentDoubleHeadCount
+fi
+
+if [ $max -eq ${doubletArray[2]} ]
+then
+	echo "TT Combination Wins with count of : " $doubleTailCount   "And Percentage Of : " $percentDoubleTailCount
+fi
+
+if [ $max -eq ${doubletArray[3]} ]
+then
+	echo "HT Combination Wins with count of : " $singleHeadTailCount   "And Percentage Of : " $percentSingleHeadTailCount
+fi
+
+if [ $max -eq ${doubletArray[4]} ]
+then
+	echo "TH Combination Wins with count of : " $singleTailHeadCount   "And Percentage Of : " $percentSingleTailHeadCount
+fi
+
+
+printf "\n"
 echo "As a simulator, do the same for Triplet Combination. (UC4)"
 printf "\n"
 
@@ -135,44 +194,44 @@ do
 	flipCoin[$i]=$(($RANDOM%8 + 1))
 	if [ ${flipCoin[$i]} -eq 1 ]
 	then
-	displayCoinTriplet[$i]="HHH"
-	HHHCount=$(($HHHCount+1))
-	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HHHCount
+		displayCoinTriplet[$i]="HHH"
+		HHHCount=$(($HHHCount+1))
+		echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HHHCount
 	elif [ ${flipCoin[$i]} -eq 2 ]
 	then
-	displayCoinTriplet[$i]="HHT"
-	HHTCount=$(($HHTCount+1))
-	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HHTCount
+		displayCoinTriplet[$i]="HHT"
+		HHTCount=$(($HHTCount+1))
+		echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HHTCount
 	elif [ ${flipCoin[$i]} -eq 3 ]
 	then
-	displayCoinTriplet[$i]="HTH"
-	HTHCount=$(($HTHCount+1)) 
-	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HTHCount
+		displayCoinTriplet[$i]="HTH"
+		HTHCount=$(($HTHCount+1)) 
+		echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HTHCount
 	elif [ ${flipCoin[$i]} -eq 4 ]
 	then
-	displayCoinTriplet[$i]="HTT"
-	HTTCount=$(($HTTCount+1))
-	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HTTCount
+		displayCoinTriplet[$i]="HTT"
+		HTTCount=$(($HTTCount+1))
+		echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $HTTCount
 	elif [ ${flipCoin[$i]} -eq 5 ]
 	then
-	displayCoinTriplet[$i]="THH"
-	THHCount=$(($THHCount+1))
-	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $THHCount
+		displayCoinTriplet[$i]="THH"
+		THHCount=$(($THHCount+1))
+		echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $THHCount
 	elif [ ${flipCoin[$i]} -eq 6 ]
 	then
-	displayCoinTriplet[$i]="THT"
-	THTCount=$(($THTCount+1))
-	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $THTCount
+		displayCoinTriplet[$i]="THT"
+		THTCount=$(($THTCount+1))
+		echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $THTCount
 	elif [ ${flipCoin[$i]} -eq 7 ]
 	then
-	displayCoinTriplet[$i]="TTH"
-	TTHCount=$(($TTHCount+1)) 
-	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $TTHCount
+		displayCoinTriplet[$i]="TTH"
+		TTHCount=$(($TTHCount+1)) 
+		echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $TTHCount
 	elif [ ${flipCoin[$i]} -eq 8 ]
 	then
-	displayCoinTriplet[$i]="TTT"
-	TTTCount=$(($TTTCount+1))
-	echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $TTTCount
+		displayCoinTriplet[$i]="TTT"
+		TTTCount=$(($TTTCount+1))
+		echo ${flipCoin[$i]}  :  ${displayCoinTriplet[$i]}  :  $TTTCount
 	fi  
 done
 
@@ -209,3 +268,70 @@ echo "percent Of THH IS : $percentOfTHH % "
 echo "percent Of THT IS : $percentOfTHT % "
 echo "percent Of TTH IS : $percentOfTTH % "
 echo "percent Of TTT IS : $percentOfTTT % "
+
+sortTriplet=$( printf "%s\n" ${flipCoin[*]} | sort -n )
+sortdisplayCoinTriplet=$( printf "%s\n" ${displayCoinTriplet[*]} | sort )
+
+echo "Displaying Outcomes Of Coins flipped is After Sorting  : " $sortTriplet
+echo "Displaying The Triplet of Coins Flipped After Sorting  : " ${sortdisplayCoinTriplet[*]}
+printf "\n"
+
+declare -A tripletArray
+tripletArray[1]=$HHHCount
+tripletArray[2]=$HHTCount
+tripletArray[3]=$HTHCount
+tripletArray[4]=$HTTCount
+tripletArray[5]=$THHCount
+tripletArray[6]=$THTCount
+tripletArray[7]=$TTHCount
+tripletArray[8]=$TTTCount
+maxTriplet=${tripletArray[1]}
+
+for (( i=1;i<9;i++ ))
+do
+	if [ ${tripletArray[$i]} -gt $maxTriplet ]
+	then
+       		maxTriplet=${tripletArray[$i]}
+  	fi
+done
+
+if [ $maxTriplet -eq ${tripletArray[1]} ]
+then
+	echo "HHH Combination Wins with count of : " $HHHCount   "And Percentage Of : " $percentOfHHH
+fi
+
+if [ $maxTriplet -eq ${tripletArray[2]} ]
+then
+	echo "HHT Combination Wins with count of : " $HHTCount   "And Percentage Of : " $percentOfHHT
+fi
+
+if [ $maxTriplet -eq ${tripletArray[3]} ]
+then
+	echo "HTH Combination Wins with count of : " $HTHCount   "And Percentage Of : " $percentOfHTH
+fi
+
+if [ $maxTriplet -eq ${tripletArray[4]} ]
+then
+	echo "HTT Combination Wins with count of : " $HTTCount   "And Percentage Of : " $percentOfHTT
+fi
+
+if [ $maxTriplet -eq ${tripletArray[5]} ]
+then
+	echo "THH Combination Wins with count of : " $THHCount   "And Percentage Of : " $percentOfTHH
+fi
+	
+if [ $maxTriplet -eq ${tripletArray[6]} ]
+then
+	echo "THT Combination Wins with count of : " $THTCount   "And Percentage Of : " $percentOfTHT
+fi
+	
+if [ $maxTriplet -eq ${tripletArray[7]} ]
+then
+	echo "TTH Combination Wins with count of : " $TTHCount   "And Percentage Of : " $percentOfTTH
+fi
+	
+if [ $maxTriplet -eq ${tripletArray[8]} ]
+then
+	echo "TTT Combination Wins with count of : " $TTTCount   "And Percentage Of : " $percentOfTTT
+fi
+	
